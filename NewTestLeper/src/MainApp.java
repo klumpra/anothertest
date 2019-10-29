daa129cc9704ea823c934e9c0b59ae9064c3e2e9
Ray Klump <raymond.klump@gmail.com>
2019-10-29 10:34:44 -0500

    Another remote change

--- NewTestLeper/src/MainApp.java
+++ NewTestLeper/src/MainApp.java
@@ -3,5 +3,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hey");
 		System.out.println("Another line");
+		System.out.println("Change made remotely");
 	}
 }
